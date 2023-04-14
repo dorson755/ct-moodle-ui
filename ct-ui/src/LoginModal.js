aed67134872c3f8da82df5f8957b9a7e578a216a
dorson755 <68614535+dorson755@users.noreply.github.com>
2023-04-14 20:41:44 +0000

    changed background and font color

--- ct-ui/src/LoginModal.js
+++ ct-ui/src/LoginModal.js
@@ -1,29 +1,51 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const LoginModal = ({ onLogin }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+const LoginModal = ({ showModal, onCloseModal, onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    onLogin(username, password);
+    try {
+      const response = await axios.post('https://cybertech242-online.com/login', {
+        username: username,
+        password: password,
+        service: 'moodle_mobile_app'
+      });
+      onLogin(response.data.token);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-    </form>
+    <div>
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={onCloseModal}>&times;</span>
+            <form onSubmit={handleLogin}>
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
